9a1b6b6d85195f91fb8b4ad9c5c4e173d1e6ee9e
tangye <ye.tang@mintegral.com>
2019-11-04 20:03:14 +0800

    slice test

--- index/main/main.go
+++ index/main/main.go
@@ -1,4 +1,4 @@
-package index
+package main
 
 /**
  * @author: tangye
@@ -16,11 +16,14 @@ import "C"
 import (
 	"fmt"
 	"github.com/Mintegral-official/juno/helpers"
+	"github.com/Mintegral-official/juno/index"
+	"sort"
 	"time"
 	"unsafe"
 )
-var s *SkipListIterator = NewSKipListIterator(DEFAULT_MAX_LEVEL, helpers.IntCompare)
+var s *index.SkipListIterator = index.NewSKipListIterator(index.DEFAULT_MAX_LEVEL, helpers.IntCompare)
 var arr [500001]int
+var s1 = make([]int, 500001)
 
 func init() {
 	b := time.Now()
@@ -31,23 +34,28 @@ func init() {
 
     a := time.Now()
 	for i := 0; i < 15000000; i++ {
-		s.Add(int(C.t()), [1]byte{})
+		//s.Add(int(C.t()), [1]byte{})
+		s1 = append(s1, int(C.t()))
 	}
 	fmt.Println(time.Since(a))
+	sort.Ints(s1)
 
 	for i := 0; i < 500000; i++ {
 		arr[i] = int(C.t())
 	}
 
-	var sl SkipList
-	var el Element
+	var sl index.SkipList
+	var el index.Element
 	fmt.Printf("Structure sizes: SkipList is %v, Element is %v bytes\n", unsafe.Sizeof(sl), unsafe.Sizeof(el))
 }
 
 func main() {
 	t := time.Now()
+
 	for j := 0; j < 500001; j++ {
-		s.Get(arr[j])
+		//s.Get(arr[j])
+		_ = s1[arr[j] % 15000000]
 	}
+
 	fmt.Println(time.Since(t))
 }
