543e3326ce59bc5bc4d5b7ca8aeebfc8ae1542a3
tangye <ye.tang@mintegral.com>
2019-12-17 17:44:17 +0800

    modify code struct

--- search/search_result.go
+++ search/search_result.go
@@ -1,11 +1,33 @@
-package index
+package search
 
 import (
 	"github.com/Mintegral-official/juno/document"
+	"github.com/Mintegral-official/juno/index"
+	"github.com/Mintegral-official/juno/query"
 	"time"
 )
 
-type SearchResult struct {
+type Result struct {
 	Docs []document.DocId
 	Time time.Duration
+}
+
+func Search(ii *index.IndexImpl, query query.Query) *Result {
+	if query == nil {
+		return nil
+	}
+	s, now := &Result{Docs: []document.DocId{}}, time.Now()
+	if _, err := query.Current(); err != nil {
+		return s
+	}
+	id, err := query.Next()
+	for err == nil {
+		if !ii.GetBitMap().IsExist(uint64(ii.GetCampaignMap()[id])) {
+			continue
+		}
+		s.Docs = append(s.Docs, id)
+		id, err = query.Next()
+	}
+	s.Time = time.Since(now)
+	return s
 }
