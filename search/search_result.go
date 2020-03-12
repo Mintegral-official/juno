a4957c7939e8be934b2b8a6dc5cf37df8b68cb78
tangye <ye.tang@mintegral.com>
2020-03-12 17:32:44 +0800

    debug

--- search/search_result.go
+++ search/search_result.go
@@ -51,13 +51,17 @@ func (s *Searcher) Debug(idx *index.Indexer, q map[string]interface{}, ids []doc
 	queryMarshal := q
 	var res = make(map[document.DocId]map[string]interface{}, len(ids))
 	for _, id := range ids {
+		tmp, ok := idx.GetCampaignMap().Get(index.DocId(id))
+		if !ok {
+			continue
+		}
 		for k, v := range queryMarshal {
 			switch k {
 			case "and", "or", "not", "and_check", "or_check", "not_and_check":
-				debugInfo(v, idx, id)
+				debugInfo(v, idx, tmp.(document.DocId))
 			case "=":
 				var termQuery = &query.TermQuery{}
-				if res, err := termQuery.Unmarshal(idx, map[string]interface{}{k: v.([]string)}).GetGE(id);
+				if res, err := termQuery.Unmarshal(idx, map[string]interface{}{k: v.([]string)}).GetGE(tmp.(document.DocId));
 					err != nil || res != id {
 					queryMarshal[k] = append(v.([]string), "id not found")
 				} else if res == id {
@@ -66,15 +70,15 @@ func (s *Searcher) Debug(idx *index.Indexer, q map[string]interface{}, ids []doc
 			case "check":
 				var c = &check.CheckerImpl{}
 				queryMarshal[k] = append(v.([]interface{}), fmt.Sprintf("check result %s",
-					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(id))))
+					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(tmp.(document.DocId)))))
 			case "in_check":
 				var c = &check.InChecker{}
 				queryMarshal[k] = append(v.([]interface{}), fmt.Sprintf("check result %s",
-					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(id))))
+					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(tmp.(document.DocId)))))
 			case "not_check":
 				var c = &check.NotChecker{}
 				queryMarshal[k] = append(v.([]interface{}), fmt.Sprintf("check result %s",
-					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(id))))
+					strconv.FormatBool(c.Unmarshal(idx, map[string]interface{}{k: v}).Check(tmp.(document.DocId)))))
 			}
 		}
 		res[id] = queryMarshal
