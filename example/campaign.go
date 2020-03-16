00f947cac69cc3d31e02ddf1b88ad1129efd2fe8
tangye <ye.tang@mintegral.com>
2020-03-16 19:08:57 +0800

    mongo index

--- example/campaign.go
+++
@@ -1,7 +0,0 @@
-package main
-
-/**
- * @author: tangye
- * @Date: 2020/3/16 16:23
- * @Description:
- */
--- example/campaign_condition.go
+++
@@ -1,7 +0,0 @@
-package main
-
-/**
- * @author: tangye
- * @Date: 2020/3/16 16:22
- * @Description:
- */
